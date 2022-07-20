42072927aea87138b4e303c780f64794aab18f55
Matteo Susanna <matteo2784@gmail.com>
2022-07-20 14:57:35 +0200

    Push

--- js/main.js
+++ js/main.js
@@ -56,38 +56,54 @@ const posts = [
     }
 ];
 
-
+let arrayId = [];
 let containerDom = document.getElementById('container');
 
 for (let i = 0; i < posts.length; i++){
     containerDom.innerHTML += `<div class="post">
-                        <div class="post__header">
-                            <div class="post-meta">                    
-                                <div class="post-meta__icon">
-                                    <img class="profile-pic" src="${posts[i].author['image']}">                    
-                                </div>
-                                <div class="post-meta__data">
-                                    <div class="post-meta__author">${posts[i].author['name']}</div>
-                                    <div class="post-meta__time">${posts[i].created}</div>
-                                </div>                    
-                            </div>
-                        </div>
-                        <div class="post__text">${posts[i].content}</div>
-                        <div class="post__image">
-                            <img src="${posts[i].media}" alt="">
-                        </div>
-                        <div class="post__footer">
-                            <div class="likes js-likes">
-                                <div class="likes__cta">
-                                    <a class="like-button  js-like-button" href="#" data-postid="1">
-                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                        <span class="like-button__label">Mi Piace</span>
-                                    </a>
-                                </div>
-                                <div class="likes__counter">
-                                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
-                                </div>
-                            </div> 
-                        </div>            
-                    </div>`
-}
+                                    <div class="post__header">
+                                        <div class="post-meta">                    
+                                            <div class="post-meta__icon">
+                                                <img class="profile-pic" src="${posts[i].author['image']}">                    
+                                            </div>
+                                            <div class="post-meta__data">
+                                                <div class="post-meta__author">${posts[i].author['name']}</div>
+                                                <div class="post-meta__time">${posts[i].created}</div>
+                                            </div>                    
+                                        </div>
+                                    </div>
+                                    <div class="post__text">${posts[i].content}</div>
+                                    <div class="post__image">
+                                        <img src="${posts[i].media}" alt="">
+                                    </div>
+                                    <div class="post__footer">
+                                        <div class="likes js-likes">
+                                            <div class="likes__cta">
+                                                <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
+                                                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                    <span class="like-button__label">Mi Piace</span>
+                                                </a>
+                                            </div>
+                                            <div class="likes__counter">
+                                                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                                            </div>
+                                        </div> 
+                                    </div>            
+                                </div>`
+};
+
+let iDom = document.querySelectorAll('.like-button__icon');
+let spanDom = document.querySelectorAll('.like-button__label');
+
+document.querySelectorAll('.likes__cta').addEventListener('click', 
+    function(event){
+        event.preventDefault();
+        iDom.style.color = 'red';
+        spanDom.style.color = 'red';
+
+    }
+);
+
+
+
+
